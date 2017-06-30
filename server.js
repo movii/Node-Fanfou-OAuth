9d94bf476fb243a078a3b8ff58b5cc84ef9836db
Lien <mo.strangeline@gmail.com>
2017-06-30 22:25:43 +0800

    switch templating engine from Jade to Nunjucks

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var m = require('./router');
+var nunjucks = require('nunjucks');
+
+// comfig nunjucks for express
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 app.disable('view cache');
 app.disable('x-powered-by');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static( __dirname + '/public'));
 
 app.use('/', m);
